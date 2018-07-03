5c41ec0a43d8e6a89dc6bb23b2684b6af1264116
martinnnachi <mnnachi@gmail.com>
2018-07-03 17:52:37 +0100

    Messages-03

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,9 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        Message message = new Message();
+        (new Thread(new Writer(message))).start();
+        (new Thread(new Reader(message))).start();
     }
 }
 
@@ -15,18 +17,30 @@ class Message {
 
     public synchronized String read() {
         while (empty) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
 
+            }
         }
         empty = true;
+        notifyAll();
         return message;
+
     }
 
     public synchronized void write(String message) {
         while (!empty) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
 
+            }
         }
         empty = false;
+        notifyAll();
         this.message = message;
+
     }
 }
 
@@ -54,8 +68,30 @@ class Writer implements Runnable {
             } catch (InterruptedException e) {
 
             }
-            message.write("finished");
+            message.write("Finished");
 
+        }
+    }
+}
+
+class Reader implements Runnable {
+    private Message message;
+
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+
+    public void run() {
+        Random random = new Random();
+        for (String latestMessage = message.read(); !latestMessage.equals("Finished");
+             latestMessage = message.read()) {
+            System.out.println(latestMessage);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException e) {
+
+            }
         }
     }
 }
